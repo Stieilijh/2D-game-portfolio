b45694c9435ad521709696985bcffa6fbc540f59
Stieilijh <saitushar02@gmail.com>
2024-04-11 13:52:36 +0530

    Changed constants.js

--- src/constants.js
+++ src/constants.js
@@ -3,15 +3,15 @@ export const scaleFactor = 4
 export const DialogueData = {
   pc: `This is my PC. I work mostly in JavaScript, Java and Python.
     Anyway regardless of the language, I just like programming. 
-    Here is my <a href="https://github.com/Stieilijh" target="_blank">Github</a>!`,
-  "cs-degree": `Here is where I will hang my B.Sc Degree.`,
-  "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
-  tv: `That's my TV.`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+    Here is my <a href="https://github.com/Stieilijh" target="_blank">Github</a>!
+    I also make posters here for my Rotaract club, Astronomy club and whenever my teachers need me to make something out of Canva.sssss `,
+  "cs-degree": `B.Sc Degree in Physics and Mathematics Loading...<br> Progress : Five Semesters Out of Six Semesters. `,
+  "sofa-table": `That's my sofa. I like to relax here with my family watching the TV.`,
+  tv: `That's my TV. I watch cricket and other sports here along with my family.`,
+  bed: `This is where I sleep. It has a 100% success rate of helping me fully recharge. I may not have invented the bed but I have perfected the art of using it`,
   resume: `This is my desk and on it is my resume. <a href="https://www.linkedin.com/in/saitushargopalakrishna/" target="_blank">Check it out?</a>
   Contact me at saitushargk@gmail.com if you have any interesting job opportunities!`,
-  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
-  library: `There are a lot of science books on my shelves.`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library and the project is inspired from JSLegendDev. Do check him out !`,
   exit: `If you want to exit my portfolio, just close the tab.`,
+  library: `I read some physics books but most books just stay here catching dust because I prefer Audiobooks.`,
 }
